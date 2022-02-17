d536af4dcbefe05b4adc650d85c926d3a48debc8
Oleg Rakovitch <devgeslot@gmail.com>
2022-02-17 16:44:37 -0500

    Add extension method for creating factory

---
+++ PartitionedStorage.Abstractions/PartitionFactoryExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Staticsoft.PartitionedStorage.Abstractions
+{
+    public static class PartitionFactoryExtensions
+    {
+        public static Partition<TData> Get<TData>(this PartitionFactory<TData> factory)
+            where TData : new()
+            => factory.Get(typeof(TData).Name);
+    }
+}
