297e2af8ee502e4f5d8242b73b8a32f027d0afcf
Oleg Rakovitch <devgeslot@gmail.com>
2022-02-17 16:59:25 -0500

    Fix extension methods

---
+++ PartitionedStorage.Abstractions/PartitionsExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Staticsoft.PartitionedStorage.Abstractions
+{
+    public static class PartitionsExtensions
+    {
+        public static Partition<TData> Get<TData>(this Partitions storage)
+            where TData : new()
+            => storage.Get<TData>(typeof(TData).Name);
+
+        public static PartitionFactory<TData> GetFactory<TData>(this Partitions storage)
+            where TData : new()
+            => storage.GetFactory<TData>(typeof(TData).Name);
+
+        public static PartitionFactory<TData> GetFactory<TData>(this Partitions storage, string partitionPrefix)
+            where TData : new()
+            => new PartitionFactory<TData>(storage, partitionPrefix);
+    }
+}
